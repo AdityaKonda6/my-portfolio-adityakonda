2cb7ad01100f65810eaa3484b35c4ad32179821e
Aditya Konda <95713208+AdityaKonda6@users.noreply.github.com>
2024-11-09 20:36:04 +0530

    Add files via upload

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            Finding creative solutions to problems is something I have always enjoyed. 🤷‍♂️
+            As a kid I loved tinkering with things that were broken, figuring out how they worked, and fixing them again. When I finished high school. 
+            I knew exactly what I wanted my career to be.
+            I'm Currently pursuing the under-graduate degree of Information Technology from K.C College of Engineering and Management Studies. 
+            I'm intrested in Web Devlopment field and seeking exciting opportunities related to the same. 
+            Being a student I have repeatedly been exposed to new culture and diverse groups of people. 
+            This is made me able to quickly adapt to changes and gain a broader perspective. 
+            I am always curious about things and enjoy learning. I like to solve problems and fix errors. 
+            I am a detail oriented person and I keep my goals and tasks organized to maintain productivity.
+              <br />
+              <br />I am fluent in classics like
+              <i>
+                <b className="purple"> React, Javascript and Python. </b>
+              </i>
+              <br />
+              <br />
+              My field of Interest's are building new &nbsp;
+              <i>
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
+                <b className="purple">
+                  Machine Learning and App Devlopment.
+                </b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also apply my passion for developing products
+              with <b className="purple">Node.js</b> and
+              <i>
+                <b className="purple">
+                  {" "}
+                  Modern Javascript Library and Frameworks
+                </b>
+              </i>
+              &nbsp; like
+              <i>
+                <b className="purple"> React.js and Next.js</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/AdityaKonda6"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/AdityaKonda7"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/aditya-adi-konda"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/konda_aditya/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+import fly from "../../Assets/Projects/airtic.png";
+import inventory from "../../Assets/Projects/1st Main Screen.png";
+import men from "../../Assets/Projects/men.png";
+import lenskart from "../../Assets/Projects/home.jpg";
+import predico from "../../Assets/Projects/Home.png";
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={fly}
+              isBlog={false}
+              title="✈️✈︎ Fly With Aditya"
+              description="A streamlined web application for booking flights, developed with PHP and MySQL. Users can search flights, book tickets, and manage bookings, while admins oversee schedules and pricing. The platform features secure user authentication, a responsive interface, and seamless transaction management."
+              ghLink="https://github.com/AdityaKonda6/Online-Flight-Booking--PHP"
+              demoLink="fly-with-aditya.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={inventory}
+              isBlog={false}
+              title="📦💰 Inventory Management System"
+              description="A desktop application in Python with SQLite for efficient inventory tracking. Users can manage products, record transactions, and generate stock reports through an intuitive GUI built with Tkinter. This system simplifies inventory processes, enhancing real-time stock monitoring and record-keeping accuracy."
+              ghLink="https://github.com/AdityaKonda6/Inventory-Management-System-Mini-Project--Python"
+              demoLink=""
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={lenskart}
+              isBlog={false}
+              title="👓 Lenskart Clone"
+              description="A React-based clone of the Lenskart website, this project simulates a responsive e-commerce platform with features like product filtering, detailed product pages, and cart functionality. It demonstrates expertise in component-driven design, providing a seamless shopping experience for desktop and mobile users alike."
+              ghLink="https://github.com/AdityaKonda6/LenskartClone-Aditya"
+              demoLink="https://lenskartclone-aditya-m1p0.onrender.com/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={men}
+              isBlog={false}
+              title="💇‍♂️ Men Saloon Management System"
+              description="A PHP-based system designed to streamline salon operations. It offers appointment booking, customer management, and service tracking for an organized client experience. With a responsive interface and admin controls, this application simplifies salon workflows, enhancing service efficiency and customer satisfaction."
+              ghLink="https://github.com/AdityaKonda6/Men-Saloon-Management-System---PHP"
+              demoLink="https://aditya-men-saloon.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={predico}
+              isBlog={false}
+              title="🩺 Predico - Disease Prediction System"
+              description="An ML-powered disease prediction platform built with Flask and Python, designed to assist in early diagnosis. Patients enter symptoms to receive predictive insights using the Naive Bayes algorithm, while doctors and admins manage records and monitor cases, enhancing healthcare accessibility and proactive intervention."
+              ghLink="https://github.com/AdityaKonda6/Predico-Aditya"
+              demoLink="predico-aditya.vercel.app"
+            />
+          </Col>
+
+        
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
