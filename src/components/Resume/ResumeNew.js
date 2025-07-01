046003afc52b911c6395cbd1bebe20d5a3b8021e
Aditya Konda <95713208+AdityaKonda6@users.noreply.github.com>
2025-07-01 21:26:44 +0530

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Aditya__Konda__Resume_Table.pdf";
+import pdf from "../../Assets/../Assets/Aditya_Konda_Resume_IT.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
