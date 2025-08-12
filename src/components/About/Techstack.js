20988953b96015001d4855db53170fb72c1102c7
Aditya Konda <adityakonda04@gmail.com>
2025-08-12 22:24:01 +0530

    modified:   src/components/About/Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -8,13 +8,18 @@ import {
   DiMongodb,
   DiPython,
   DiJava,
+  DiJenkins,
+  DiDocker,
 } from "react-icons/di";
 import {
-
   SiFirebase,
- 
+  SiSelenium,
+  SiCypress,
+  SiPostman,
+  SiKubernetes,
+  SiAnsible,
+  SiGithubactions,
 } from "react-icons/si";
-
 
 function Techstack() {
   return (
@@ -25,29 +30,51 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-    
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
       </Col>
-    
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
-     
-      
       <Col xs={4} md={2} className="tech-icons">
         <SiFirebase />
       </Col>
-     
-     
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+      </Col>
+      
+      {/* Added Automation Testing Tools */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSelenium />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCypress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+
+      {/* Added DevOps Tools */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJenkins />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiKubernetes />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAnsible />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGithubactions />
       </Col>
     </Row>
   );
