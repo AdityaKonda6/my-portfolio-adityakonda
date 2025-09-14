0cb24e56c94973743d5ed60cf6581d0cb8d22ff2
Aditya Konda <95713208+AdityaKonda6@users.noreply.github.com>
2025-09-14 23:54:38 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,82 @@
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import {
+//   AiFillGithub,
+//   AiOutlineTwitter,
+//   AiFillInstagram,
+// } from "react-icons/ai";
+// import { FaLinkedinIn } from "react-icons/fa";
+
+// function Footer() {
+//   let date = new Date();
+//   let year = date.getFullYear();
+//   return (
+//     <Container fluid className="footer">
+//       <Row>
+//         <Col md="4" className="footer-copywright">
+//           <h3>Designed and Developed by Aditya Konda</h3>
+//         </Col>
+//         <Col md="4" className="footer-copywright">
+//           <h3>Copyright © {year} AK</h3>
+//         </Col>
+//         <Col md="4" className="footer-body">
+//           <ul className="footer-icons">
+//             <li className="social-icons">
+//               <a
+//                 href="https://github.com/AdityaKonda6"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillGithub />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://twitter.com/AdityaKonda7"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiOutlineTwitter />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.linkedin.com/in/aditya-adi-konda"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <FaLinkedinIn />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.instagram.com/konda_aditya/"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillInstagram />
+//               </a>
+//             </li>
+//           </ul>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default Footer;
+
+
+
+
+
+
+
+
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
@@ -6,6 +85,7 @@ import {
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import { CgWebsite } from "react-icons/cg"; //  <-- Import the new icon
 
 function Footer() {
   let date = new Date();
@@ -61,6 +141,17 @@ function Footer() {
                 <AiFillInstagram />
               </a>
             </li>
+            {/* This is the new link you wanted to add */}
+            <li className="social-icons">
+              <a
+                href="https://jiffyjugaad.in/"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <CgWebsite />
+              </a>
+            </li>
           </ul>
         </Col>
       </Row>
@@ -69,8 +160,3 @@ function Footer() {
 }
 
 export default Footer;
-
-
-
-
-
